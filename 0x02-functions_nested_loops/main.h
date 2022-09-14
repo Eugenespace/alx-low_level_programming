0e1333ca4ecda09d8f0839ec0924a94919e27c79
Eugenespace <eugene_1@live.co.uk>
2022-09-14 16:13:33 +0000

    main.h created

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _alxProgramming_H_
+#define _alxProgramming_H_
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif /* _alxProgramming_H_ */
