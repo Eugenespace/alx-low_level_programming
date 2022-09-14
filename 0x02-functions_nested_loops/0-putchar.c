a4eb95d05440b1af5712c58f0d67004bf522fa74
Eugenespace <eugene_1@live.co.uk>
2022-09-14 16:40:03 +0000

    line at the end added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
-/**                                                                                                                   
- * main - Prints putchar followed by a new line                                                                       
- *                                                                                                                    
- * Return: Always 0 (Success)                                                                                         
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 _putchar('_');
@@ -14,6 +16,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar('\n');
 return (0);
 }
-
