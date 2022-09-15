751ada66e750ccbbc022e3bf0bb45fd4bc8b9102
Eugenespace <eugene_1@live.co.uk>
2022-09-15 19:39:32 +0000

    multiple two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiples two intergers
+ *
+ *@a: First interger
+ *@b: First interger
+ *
+ *Return: multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+  
+return ( a * b);
+}
