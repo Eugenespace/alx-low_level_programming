d4705a3b9cbc8d02e2e1b728ca3f2a47742c34db
Eugene Kartey <eugene_1@live.co.uk>
2022-10-10 22:32:11 +0000

    betty resolved

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,16 +3,16 @@
 #include<stdio.h>
 
 /**
- * struct dog - structure of dog object
+ * init_dog- structure of dog object
  * @name:name of dog
  * @owner: owner of dog
  * @age: age of dog
+ * @d:pointer to the dog object in memory
  */
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
 	if (d != NULL)
 	{
 		d->name = name;
