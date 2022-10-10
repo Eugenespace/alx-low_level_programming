5d87ae21ed96c74747b21f1d81ac79bd6b5955ba
Eugene Kartey <eugene_1@live.co.uk>
2022-10-10 21:32:06 +0000

    dog h created

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,44 +3,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
 /**
- *struct dog- structure of dog object
- *@name: name of dog
- *@owner: owner of dog
- *@age: age of dog
- *Return: my_dog
+ * struct dog - info on dog.
+ * @name: dog's name.
+ * @age: age of the dog in float.
+ * @owner: name of the owner of the dog.
+ *
  */
 
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
 
-struct dog *my_dog(char *name, char *owner, float age)
-{
-	struct dog  *my_dog;
-
-	my_dog = malloc(sizeof(struct dog));
-	printf("%lu\n", sizeof(struct dog));
-	if (my_dog == NULL)
-		return (NULL);
-	my_dog->name = name;
-	my_dog->owner = owner;
-	my_dog->age = age;
-
-	return (my_dog);
-	free(my_dog);
-}
-
-
-
-
-
-
-
-
+typedef struct dog my_dog;
+my_dog *new_dog(char *name, float age, char *owner);
+void free_dog(my_dog *d);
 
 
 #endif
