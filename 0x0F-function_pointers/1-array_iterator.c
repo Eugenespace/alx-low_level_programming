4e17d17094a2601099ffc6577fff450f62114b3d
Eugene Kartey <eugene_1@live.co.uk>
2022-10-12 20:55:45 +0000

    iteration done of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
-
-
-
-
+/**
+ * array_iterator- iterates the array
+ * @action:function pointer
+ * @array:array to iterate
+ * @size: size of array
+ *
+ * Return: Always 0
+ */
 
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	(*array)(*action);
+	unsigned long i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
